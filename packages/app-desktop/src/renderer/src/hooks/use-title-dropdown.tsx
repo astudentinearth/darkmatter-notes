18cae1b8a0dba7f02be99e8e8fb0d824d3f60f66
Burak <burak.yn.dev@gmail.com>
2024-11-11 23:52:42 +0300

    ♻️ Refactor title hook to extract logic

--- packages/app-desktop/src/renderer/src/hooks/use-title-dropdown.tsx
+++ packages/app-desktop/src/renderer/src/hooks/use-title-dropdown.tsx
@@ -6,6 +6,59 @@ import { useLocation } from "react-router-dom";
 import { useNotesQuery } from "./query";
 import { useNoteFromURL } from "./use-note-from-url";
 
+function getTitle(
+    notes: Note[] | undefined,
+    isLoading: boolean,
+    path: string,
+    pageId: string | undefined,
+) {
+    if (notes == null || isLoading) {
+        return "Loading";
+    }
+    if (pageId == null && !path.startsWith("/page")) {
+        switch (path) {
+            case "/": {
+                return (
+                    <>
+                        <Home size={18} />
+                        &nbsp;Home
+                    </>
+                );
+            }
+            case "/settings": {
+                return (
+                    <>
+                        <Settings size={18} />
+                        &nbsp;Settings
+                    </>
+                );
+            }
+            default:
+                return "";
+        }
+    } else {
+        const note = notes.find((n) => n.id === pageId);
+        if (note) {
+            return (
+                <>
+                    <ChevronDown size={18} />
+                    <div className="text-lg translate-y-[-5%]">
+                        {fromUnicode(note.icon)}
+                    </div>
+                    {note.title}
+                </>
+            );
+        } else return "";
+    }
+}
+
+function resolveUpperTree(notes: Note[], id: string): Note[] {
+    const note = notes.find((n) => n.id === id);
+    if (!note || !note.parentID) return [];
+    const nodes: Note[] = resolveParents(id, notes);
+    return nodes;
+}
+
 export const useTitleDropdown = () => {
     const id = useNoteFromURL();
     const location = useLocation();
@@ -16,54 +69,15 @@ export const useTitleDropdown = () => {
     const [title, setTitle] = useState<ReactNode>("Darkwrite");
 
     useEffect(() => {
-        if (notes == null || isLoading) {
-            setTitle("Loading...");
-            return;
-        }
         const path = location.pathname;
-        const resolveUpperTree = (note: Note) => {
-            if (note.parentID == null) setParentNodes([]);
-            const nodes: Note[] = resolveParents(note.id, notes);
-            setParentNodes(nodes);
-        };
-        if (id == null && !path.startsWith("/page")) {
-            switch (path) {
-                case "/": {
-                    setTitle(
-                        <>
-                            <Home size={18} />
-                            &nbsp;Home
-                        </>,
-                    );
-                    break;
-                }
-                case "/settings": {
-                    setTitle(
-                        <>
-                            <Settings size={18} />
-                            &nbsp;Settings
-                        </>,
-                    );
-                    break;
-                }
-            }
-            setParentNodes([]);
-        } else {
-            // set title
-            const note = notes.find((n) => n.id === id);
-            if (note) {
-                resolveUpperTree(note);
-                setTitle(
-                    <>
-                        <ChevronDown size={18} />
-                        <div className="text-lg translate-y-[-5%]">
-                            {fromUnicode(note.icon)}
-                        </div>
-                        {note.title}
-                    </>,
-                );
-            }
-        }
+        const title = getTitle(notes, isLoading, path, id);
+        setTitle(title);
+    }, [id, isLoading, location.pathname, notes]);
+
+    useEffect(() => {
+        const path = location.pathname;
+        if (!path.startsWith("/page") || !notes || !id) setParentNodes([]);
+        else setParentNodes(resolveUpperTree(notes, id));
     }, [location, notes, isLoading, id]);
     return { parentNodes, title };
 };

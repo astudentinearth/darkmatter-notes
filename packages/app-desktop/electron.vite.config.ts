8a805e7923af455fa8e104786a4ee819028f2484
Burak <burak.yn.dev@gmail.com>
2024-11-18 20:49:46 +0300

    🚚 Move stuff around - just around i promise

--- packages/app-desktop/electron.vite.config.ts
+++ packages/app-desktop/electron.vite.config.ts
@@ -1,33 +1,36 @@
-import react from "@vitejs/plugin-react"
-import {defineConfig, externalizeDepsPlugin} from "electron-vite"
+import react from "@vitejs/plugin-react";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import path from "path";
 export default defineConfig({
-    main:{
-        plugins: [externalizeDepsPlugin()],
-        resolve:{
-            alias:{
-                "@common": path.resolve("src/common")
-            }
-        }
-    },
-    preload: {
-        plugins: [externalizeDepsPlugin()],
-        build:{
+    main: {
+        plugins: [],
+        build: {
             target: "node20",
             rollupOptions: {
                 output: {
-                    format: "cjs"
-                }
-            }
-        }
+                    format: "cjs",
+                },
+            },
+        },
+    },
+    preload: {
+        plugins: [externalizeDepsPlugin()],
+        build: {
+            target: "node20",
+            rollupOptions: {
+                output: {
+                    format: "cjs",
+                },
+            },
+        },
     },
     renderer: {
         plugins: [react()],
-        resolve:{
-            alias:{
-                "@renderer":path.resolve("src/renderer/src"),
-                "@common": path.resolve("src/common")
-            }
-        }
+        resolve: {
+            alias: {
+                "@renderer": path.resolve("src/renderer/src"),
+                "@common": path.resolve("src/common"),
+            },
+        },
     },
 });

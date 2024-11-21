276e1fdd71917842750c7fa34f4e64a6a6373ad3
Burak <burak.yn.dev@gmail.com>
2024-11-22 00:39:42 +0300

    add dim theme

--- packages/app-desktop/src/lib/themes/index.ts
+++ packages/app-desktop/src/lib/themes/index.ts
@@ -1,4 +1,5 @@
 import { CatppuccinMocha } from "./catppuccin";
+import { DarkwriteDim } from "./darkwrite-black";
 import { DarkwriteDefault } from "./darkwrite-default";
 
-export const DEFAULT_THEMES = [DarkwriteDefault, CatppuccinMocha];
+export const DEFAULT_THEMES = [DarkwriteDefault, CatppuccinMocha, DarkwriteDim];

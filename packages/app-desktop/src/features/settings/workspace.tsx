faee6347cdceaad72c2cb7ff3ec2b3ea67b317a8
Burak <burak.yn.dev@gmail.com>
2024-11-21 19:22:55 +0300

    move full workspace export to a mutation

--- packages/app-desktop/src/features/settings/workspace.tsx
+++ packages/app-desktop/src/features/settings/workspace.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@renderer/components/ui/button";
-import { ExporterModel } from "@renderer/lib/api/exporter";
+import { useWorkspaceExport } from "@renderer/hooks/query";
 
 export function WorkspaceSettings() {
+    const workspaceExport = useWorkspaceExport();
     return (
         <div className="p-4 bg-card/80 rounded-2xl grid grid-cols-[1fr_auto] items-center auto-rows-auto gap-4">
             <h1 className="text-lg font-semibold text-foreground/75 col-span-2">
@@ -11,11 +12,14 @@ export function WorkspaceSettings() {
             <span>Export all notes as HTML</span>
             {/** Move this to a mutation for pending state */}
             <Button
-                onClick={() => ExporterModel.exportAllAsHTML()}
+                onClick={() => workspaceExport.mutateAsync()}
+                disabled={workspaceExport.isPending}
                 variant={"secondary"}
                 className="flex-shrink-0 w-fit place-self-end"
             >
-                Export all notes
+                {workspaceExport.isPending
+                    ? "Exporting..."
+                    : "Export all notes"}
             </Button>
             <span>Backup and restore data</span>
             <div className="flex gap-2 place-self-end">
---
+++ packages/app-desktop/src/hooks/query/use-workspace-export.ts
@@ -0,0 +1,9 @@
+import { ExporterModel } from "@renderer/lib/api/exporter";
+import { useMutation } from "@tanstack/react-query";
+
+export const useWorkspaceExport = () => {
+    return useMutation({
+        mutationKey: ["workspace-export"],
+        mutationFn: ExporterModel.exportAllAsHTML,
+    });
+};

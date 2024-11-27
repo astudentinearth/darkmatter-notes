071795075742293b1edc5f3a2ebe418c96696568
Burak <burak.yn.dev@gmail.com>
2024-11-27 15:27:00 +0300

    remove the image resizer which should not have existed

--- packages/app-desktop/src/features/editor/text-editor.tsx
+++ packages/app-desktop/src/features/editor/text-editor.tsx
@@ -2,55 +2,53 @@ import { NoteCustomizations } from "@darkwrite/common";
 import { defaultExtensions } from "@renderer/features/editor/extensions/extensions";
 import { cn } from "@renderer/lib/utils";
 import { EditorContent, EditorRoot, JSONContent } from "novel";
-import { handleCommandNavigation, ImageResizer } from "novel/extensions";
+import { handleCommandNavigation } from "novel/extensions";
 import Bubble from "./bubble-menu";
 import SlashCommand from "./command-menu";
 import { slashCommand } from "./command-menu/slash-command";
 import InstanceHandler from "./instance-handler";
 
 interface TextEditorProps {
-    initialValue?: JSONContent;
-    onChange?: (content: JSONContent) => void;
-    customizations: NoteCustomizations;
+  initialValue?: JSONContent;
+  onChange?: (content: JSONContent) => void;
+  customizations: NoteCustomizations;
 }
 
 const extensions = [...defaultExtensions, slashCommand];
 
 export const TextEditor = ({ initialValue, onChange }: TextEditorProps) => {
-    return (
-        <EditorRoot>
-            <EditorContent
-                className={cn(
-                    "p-0 px-16 rounded-xl w-full max-w-[--editor-max-width] flex-grow break-words transition-transform",
-                )}
-                {...(initialValue && { initialContent: initialValue })}
-                extensions={extensions}
-                editorProps={{
-                    handleDOMEvents: {
-                        keydown: (_view, event) =>
-                            handleCommandNavigation(event),
-                    },
-                    attributes: {
-                        class: cn(
-                            `prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full text-[--dw-editor-foreground]`,
-                        ),
-                    },
-                    // TODO: Come back here later
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    handleDrop: (_view, _event) => {
-                        //if (event.dataTransfer?.types.includes("note_id"))
-                        //console.log("Linking to note");
-                    },
-                }}
-                onUpdate={({ editor }) => {
-                    onChange?.call(null, editor.getJSON());
-                }}
-                slotAfter={<ImageResizer />}
-            >
-                <InstanceHandler />
-                <SlashCommand />
-                <Bubble />
-            </EditorContent>
-        </EditorRoot>
-    );
+  return (
+    <EditorRoot>
+      <EditorContent
+        className={cn(
+          "p-0 px-16 rounded-xl w-full max-w-[--editor-max-width] flex-grow break-words transition-transform",
+        )}
+        {...(initialValue && { initialContent: initialValue })}
+        extensions={extensions}
+        editorProps={{
+          handleDOMEvents: {
+            keydown: (_view, event) => handleCommandNavigation(event),
+          },
+          attributes: {
+            class: cn(
+              `prose prose-lg dark:prose-invert prose-headings:font-title font-default focus:outline-none max-w-full text-[--dw-editor-foreground]`,
+            ),
+          },
+          // TODO: Come back here later
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          handleDrop: (_view, _event) => {
+            //if (event.dataTransfer?.types.includes("note_id"))
+            //console.log("Linking to note");
+          },
+        }}
+        onUpdate={({ editor }) => {
+          onChange?.call(null, editor.getJSON());
+        }}
+      >
+        <InstanceHandler />
+        <SlashCommand />
+        <Bubble />
+      </EditorContent>
+    </EditorRoot>
+  );
 };

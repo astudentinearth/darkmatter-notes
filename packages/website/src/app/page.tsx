dec61a99ac81db35bbcdd4b3e3f0d8b9ec8e45bc
Burak <burak.yn.dev@gmail.com>
2025-02-09 23:30:54 +0300

    build landing page

--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/card";
 import { Nav } from "@/components/nav";
 import Image from "next/image";
 import Link from "next/link";
@@ -42,10 +43,10 @@ export default function Home() {
             width={1920}
             height={643}
             alt="landing"
-            src="/landing.png"
+            src="/landing_image.png"
           />
         </div>
-        <div className="flex flex-col bg-alternate w-full items-center pt-16">
+        <div className="flex flex-col bg-alternate w-full items-center pt-16 pb-16 px-6">
           <h1 className="text-center text-5xl font-bold">
             Type it out with <span className="text-theme-3">ease.</span>
           </h1>
@@ -58,7 +59,56 @@ export default function Home() {
             <span className="text-theme-4">text formatting, </span>
             and <span className="text-theme-3">images.</span>
           </span>
-          <Image src="/editor.png" alt="editor" width={882} height={638}/>
+          <Image
+            className="rounded-[8px] max-w-[1000px] w-full border border-[#434343] drop-shadow-lg mt-5"
+            src="/img2.png"
+            alt="editor"
+            width={1000}
+            height={992}
+          />
+        </div>
+        <div className="flex flex-col w-full items-center pt-16 pb-16 px-6">
+          <h1 className="text-center text-5xl font-bold">
+            <span className="text-theme-4">Co</span>
+            <span className="text-theme-3">lor</span>
+            <span className="text-theme-2">ful,</span>
+            <span className="text-theme-1"> really.</span>
+          </h1>
+          <span className="text-center text-2xl mt-3">
+            Pick the theme and font you love the most. Everything is up to you.
+          </span>
+          <Image
+            className="w-full mt-5 max-w-[900px]"
+            src={"/themes.png"}
+            alt="themes"
+            width={900}
+            height={777}
+          ></Image>
+        </div>
+        <div className="w-full bg-alternate py-16 px-6 flex flex-col items-center">
+          <h1 className="text-center text-5xl font-bold">
+            Open and <span className="text-theme-2">private.</span>
+          </h1>
+          <div className="flex flex-col gap-4 mt-6 max-w-[600px]">
+            <Card className="flex flex-col gap-2 w-full">
+              <span className="font-semibold text-3xl">No server.</span>
+              <span className="text-xl opacity-80">
+                Everything is offline. Never worry about losing access.
+              </span>
+            </Card>
+            <Card className="flex flex-col gap-2 w-full">
+              <span className="font-semibold text-3xl">No data collected.</span>
+              <span className="text-xl opacity-80">
+                Only you will know what&apos;s written in those pages.
+              </span>
+            </Card>
+            <Card className="flex flex-col gap-2 w-full">
+              <span className="font-semibold text-3xl">Open source.</span>
+              <span className="text-xl opacity-80">
+              You get to know what you run on your device.
+              </span>
+            </Card>
+          </div>
         </div>
       </main>
       <footer className=""></footer>
--- packages/website/src/components/nav.tsx
+++ packages/website/src/components/nav.tsx
@@ -7,15 +7,12 @@ export async function Nav() {
       <Link href={"/"}>
         <Image src={"/darkwrite_icon.png"} alt="Logo" width={40} height={40} />
       </Link>
-      <div className="h-10 flex justify-center items-center p-3 select-none hover:bg-white/10 transition-colors rounded-[6px]">
-        Resources
-        <div className="absolute hidden bg-nav border-[#c1c1c1]/25 p-3 border rounded-[18px] top-[4.5rem]">
-          User guide
-        </div>
-      </div>
+      <Link href={"https://github.com/astudentinearth/darkwrite"} className="h-10 flex cursor-default justify-center items-center p-3 select-none hover:bg-white/10 transition-colors rounded-[6px]">
+        Source code
+      </Link>
       <Link
         href={"/download"}
-        className="bg-primary h-10 p-3 flex items-center justify-center rounded-[6px] hover:brightness-125 transition-[filter]"
+        className="bg-primary h-10 p-3 flex items-center cursor-default justify-center rounded-[6px] hover:brightness-125 transition-[filter]"
       >
         Download
       </Link>

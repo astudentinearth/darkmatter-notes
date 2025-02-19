ae294786a43020c79c392bbab845a293f7ab1249
Burak <burak.yn.dev@gmail.com>
2025-02-19 14:56:07 +0300

    🔨 Add env switch to run development server without electron

--- docs/DEVELOPMENT.md
+++ docs/DEVELOPMENT.md
@@ -4,6 +4,9 @@
 
 Install packages by invoking `yarn` at the root of the source tree. When the dependencies are installed, run `yarn dev`.
 
+> [!TIP]  
+> Set the environment variable `DW_WITHOUT_ELECTRON` to `1` if you want to run the frontend development server only, without launching Electron.
+
 ## Tooling
 
 Darkwrite uses `yarn` for package management. `vite-plugin-electron` is used to bundle the app. Changes to source code triggers hot module reload during development. Changes to main process code automatically restarts the app.
--- packages/app-desktop/vite.config.ts
+++ packages/app-desktop/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import electron from "vite-plugin-electron/simple";
-import tailwind from "@tailwindcss/vite"
+import tailwind from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [
-    react(),
+const plugins: PluginOption[] = [react(), tailwind()];
+
+if (process.env["DW_WITHOUT_ELECTRON"] != "1")
+  plugins.push(
     electron({
       main: {
         entry: path.resolve("src/electron/main.ts"),
@@ -27,8 +28,10 @@ export default defineConfig({
         input: path.resolve("src/electron/preload/preload.ts"),
       },
     }),
-    tailwind()
-  ],
+  );
+
+export default defineConfig({
+  plugins,
   resolve: {
     alias: {
       "@renderer": path.resolve("src/"),
